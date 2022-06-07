f06d2171a5016f132cbc7c6a2dbf0bce43801798
Anton <95871478+antonbertrand-atos@users.noreply.github.com>
2022-06-07 20:47:21 +0100

    Implemented 'Play a Premade Quiz' functionality using a public API

--- src/main/java/com/example/RevisionApp/Quiz.java
+++ src/main/java/com/example/RevisionApp/Quiz.java
@@ -1,5 +1,11 @@
 package com.example.RevisionApp;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -60,6 +66,104 @@ public class Quiz {
 
     public void addQuizQuestions(QuizQuestions quizQuestions) {
         this.quizQuestions.add(quizQuestions);
+    }
+
+    public static JSONArray quizAPI() {
+
+        System.out.println("Choose a Quiz Topic:" +
+                "\n" + "1) Geography" +
+                "\n" + "2) Sports" +
+                "\n" + "3) History");
+
+        Scanner scanner2 = new Scanner(System.in);
+        int category = 0;
+        String input = scanner2.nextLine();
+
+        switch(input) {
+            case "1":
+                category = 22;
+                break;
+            case "2":
+                category = 21;
+                break;
+            case "3":
+                category = 23;
+                break;
+        }
+
+
+        try {
+
+            URL url = new URL("https://opentdb.com/api.php?amount=10&category="+category+"&difficulty=easy&type=boolean");
+
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            //Check if connect is made
+            int responseCode = conn.getResponseCode();
+
+            // 200 OK
+            if (responseCode != 200) {
+                throw new RuntimeException("HttpResponseCode: " + responseCode);
+            } else {
+
+                StringBuilder informationString = new StringBuilder();
+                Scanner scanner = new Scanner(url.openStream());
+
+                while (scanner.hasNext()) {
+                    informationString.append(scanner.nextLine());
+                }
+                //Close the scanner
+                scanner.close();
+
+                informationString.delete(0,29);
+                informationString.delete(informationString.length()-1,informationString.length());
+
+                //JSON simple library Setup with Maven is used to convert strings to JSON
+                JSONParser parse = new JSONParser();
+                JSONArray dataObject = (JSONArray) parse.parse(String.valueOf(informationString));
+
+                return dataObject;
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //To Stop method throwing an error for missing return statement
+        JSONArray array = null;
+        return array;
+    }
+
+    public static void startPremadeQuiz(JSONArray premadeQuiz) {
+
+        String question;
+        String answer;
+        int score = 0;
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        System.out.println("Quiz Size: 10");
+
+
+
+        for (int i = 0; i < 10;  i++) {
+
+
+            JSONObject premadeQuiz2 = (JSONObject) premadeQuiz.get(i);
+            System.out.println("Question " + (i+1) +": " + premadeQuiz2.get("question"));
+
+            input = scanner.nextLine();
+            if(input.equals(premadeQuiz2.get("correct_answer"))) {
+                System.out.println("Correct!");
+                score++;
+            } else {
+                System.out.println("WRONG! - Correct answer was: " + premadeQuiz2.get("correct_answer"));
+            }
+        }
+
+        System.out.println("Quiz Over! Your final score was: " + score + "/10");
     }
 
 }
--- src/main/java/com/example/RevisionApp/RevisionAppApplication.java
+++ src/main/java/com/example/RevisionApp/RevisionAppApplication.java
@@ -1,69 +1,51 @@
 package com.example.RevisionApp;
 
+import org.json.simple.JSONArray;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Scanner;
 
+import static com.example.RevisionApp.Quiz.quizAPI;
+import static com.example.RevisionApp.Quiz.startPremadeQuiz;
+
 @SpringBootApplication
 public class RevisionAppApplication {
+
+	static Quiz quiz = new Quiz("Test");
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(RevisionAppApplication.class, args);
 
 		boolean flag = false;
-		Quiz quiz = new Quiz("Test");
+
 		Scanner scanner = new Scanner(System.in);
 
 		while (!flag) {
 			System.out.println("Available Options: " +
-			"\n" + "1) Create a new Quiz" +
+					"\n" + "1) Create a new Quiz" +
 					"\n" + "2) List all Quiz's" +
-					"\n" + "3) Play a Quiz");
+					"\n" + "3) Play a Custom Quiz" +
+					"\n" + "4) Play a Pre-Made Quiz" +
+					"\n" + "5) Quit");
 			System.out.println("Please Enter a Command");
 			String input = scanner.nextLine();
 
 			switch (input) {
+
+				     //Create a new Quiz
 				case "1":
-
-					QuizQuestions quizQuestions = new QuizQuestions();
-					boolean flag2 = true;
-					while (flag2) {
-
-						System.out.println("Enter your question");
-						String input2 = scanner.nextLine();
-						Questions question = new Questions(input2);
-
-						System.out.println("Enter the answer");
-						input2 = scanner.nextLine();
-						Answers answer = new Answers(input2);
-
-						quizQuestions.setQuestions(question, answer);
-
-						System.out.println("Questions:");
-						quizQuestions.readQuestions();
-
-						System.out.println("Add another question? Y/N");
-						input2 = scanner.nextLine();
-
-						if (input2.equals("N")) {
-							System.out.println("Enter a Quiz Name");
-							input2 = scanner.nextLine();
-							quizQuestions.setName(input2);
-
-							quiz.addQuizQuestions(quizQuestions);
-							break;
-						}
-
-					}
-
-
+					createQuiz();
 					break;
+
+					//List Of Custom Quiz's
 				case "2":
 					System.out.println("List of Quizzes:");
 					quiz.readQuizQuestions();
 					break;
 
+					//Play a Custom Quiz
 				case "3":
 					System.out.println("Choose a Quiz:");
 					quiz.readQuizQuestions();
@@ -72,11 +54,57 @@ public class RevisionAppApplication {
 
 
 					break;
+
+					//Play a Pre-made Quiz
+				case "4":
+					JSONArray test = quizAPI();
+					startPremadeQuiz(test);
+					break;
+
+					//Quit the application
+				case "5":
+					System.exit(0);
+					break;
 			}
 
 		}
 
 
 	}
+
+	public static void createQuiz () {
+		Scanner scanner = new Scanner(System.in);
+		QuizQuestions quizQuestions = new QuizQuestions();
+		boolean flag2 = true;
+		while (flag2) {
+
+			System.out.println("Enter your question");
+			String input2 = scanner.nextLine();
+			Questions question = new Questions(input2);
+
+			System.out.println("Enter the answer");
+			input2 = scanner.nextLine();
+			Answers answer = new Answers(input2);
+
+			quizQuestions.setQuestions(question, answer);
+
+			System.out.println("Questions:");
+			quizQuestions.readQuestions();
+
+			System.out.println("Add another question? Y/N");
+			input2 = scanner.nextLine();
+
+			if (input2.equals("N")) {
+				System.out.println("Enter a Quiz Name");
+				input2 = scanner.nextLine();
+				quizQuestions.setName(input2);
+
+				quiz.addQuizQuestions(quizQuestions);
+				break;
+			}
+
+		}
+	}
+
 
 }
